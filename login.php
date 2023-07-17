a9588937f84c4f582ac55a772de29ca0b7500adf
MaximeG9 <gurbala.maxime@gmail.com>
2023-07-17 15:49:36 +0200

    create login.php and login_trait.php

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex justify-content-center position-absolute top-50 start-50 translate-middle" method="post">
+            <form action="./process/login_trait.php" method="post" enctype="multipart/form-data">
+                <div>
+                    <div class="d-flex justify-content-center">
+                        <h1>BIENVENUE SUR</h1>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <img src="./assets/logo-insta.png" alt="logo Instagram" class="instagram-logo">
+                    </div>
+                    <div class="my-3">
+                        <input type="text" class="form-control" placeholder="nom d'utilisateur" name="username" id="">
+                    </div>
+
+                    <div class="my-3">
+                        <input type="text" class="form-control" placeholder="pseudo" name="pseudo" id="">
+                    </div>
+
+                    <div class="mb-2">
+                        <div class="d-flex justify-content-center my-3">
+                            <label for="avatar">Choisissez une photo de profil :</label>
+                        </div>
+
+                        <div class="d-flex justify-content-center my-3">
+                            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">
+                        </div>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-dark btn-lg">connexion</button>
+                    </div>
+
+                </div>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
