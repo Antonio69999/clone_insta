3367ec5fb225adf17146c62ac2b2eb10ac1fd050
MaximeG9 <gurbala.maxime@gmail.com>
2023-07-18 11:28:12 +0200

    login finished

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php require_once("./utils/connexion.php"); ?>
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,12 +8,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="d-flex justify-content-center position-absolute top-50 start-50 translate-middle" method="post">
-            <form action="./process/login_trait.php" method="post" enctype="multipart/form-data">
+            <form action="./process/login_trait.php" method="post">
                 <div>
                     <div class="d-flex justify-content-center">
                         <h1>BIENVENUE SUR</h1>
@@ -23,17 +28,7 @@
                     </div>
 
                     <div class="my-3">
-                        <input type="text" class="form-control" placeholder="pseudo" name="pseudo" id="">
-                    </div>
-
-                    <div class="mb-2">
-                        <div class="d-flex justify-content-center my-3">
-                            <label for="avatar">Choisissez une photo de profil :</label>
-                        </div>
-
-                        <div class="d-flex justify-content-center my-3">
-                            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">
-                        </div>
+                        <input type="text" class="form-control" placeholder="pseudo" name="pseudos" id="">
                     </div>
 
                     <div class="d-flex justify-content-center">
--- process/login_trait.php
+++ process/login_trait.php
@@ -1,36 +1,35 @@
-<?php session_start(); ?>
 <?php require_once('../utils/connexion.php'); ?>
+<?php session_start(); ?>
 
 
 <?php
 
-if (
-    isset($_POST['username']) && !empty($_POST['username'])
-    && isset($_POST['pseudos']) && !empty($_POST['pseudos'])
-) {
+if (!empty($_POST['username'])
+    && !empty($_POST['pseudos'])){
     $user = $_POST['username'];
     $selectUserQuery = $db->prepare("SELECT * FROM users WHERE username = :username");
     $selectUserQuery->execute([':username' => $user]);
     $user = $selectUserQuery->fetch();
     if ($user) {
         $_SESSION['user'] = $user;
-        header('Location: ../index.php');
+        header('Location: ../instagram.php');
         // le nom d'utilisateur existe déjà
     } else {
         // le nom d'utilisateur n'existe pas
-        $insertUserSql = "INSERT INTO users (username, pseudos) VALUES (:pseudos, :username)";
+        $insertUserSql = "INSERT INTO users (username, pseudos) VALUES (:username, :pseudos)";
         $insertUserQuery = $db->prepare($insertUserSql);
         $insertUserQuery->execute([
-            ':pseudos' => $_POST['pseudos'],
             ':username' => $_POST['username'],
+            ':pseudos' => $_POST['pseudos'],
         ]);
         $userId = $db->lastInsertId();
-        $_SESSION['user']['pseudos'] = $_POST['pseudos'];
         $_SESSION['user']['username'] = $_POST['username'];
-        $_SESSION['user']['id_user'] = $userId;
-        var_dump($_SESSION);
-        header('Location: ../index.php');
+        $_SESSION['user']['pseudos'] = $_POST['pseudos'];
+        $_SESSION['user']['id_users'] = $userId;
+        // var_dump($_SESSION);
+        header('Location: ../instagram.php');
     }
 }
 
 ?>
+
